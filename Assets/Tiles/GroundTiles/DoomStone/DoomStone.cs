e5b01e277534e5ec92ca5947e30daa08e8dbe034
Virex22 <vincentremy222@gmail.com>
2023-02-16 23:19:03 +0100

    feat : add doomstone tile to project

---
+++ Assets/Tiles/GroundTiles/DoomStone/DoomStone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tiles
+{
+    public class DoomStone : Tiles
+    {
+        public void Awake()
+        {
+            sprite = Resources.Load<Sprite>("Tiles/Doomstone");
+            Init();
+        }
+    }
+}
